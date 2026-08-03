cbc296a418c0908e08a232caf50debd8cee14ce8
Yasir Shabbir <yasirshabbir44@gmail.com>
2026-08-04 00:37:13 +0400

    Initial Commit

---
+++ iSmail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iSmail
+//
+//  Created by Yasir Shabbir on 04/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iSmail/iSmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSmailApp.swift
+//  iSmail
+//
+//  Created by Yasir Shabbir on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct iSmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
